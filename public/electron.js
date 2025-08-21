4b2f239c624e407c56c374bafe2637bf58c418f1
Pooja Gajera <poojagajera@Mac.lan>
2025-08-21 15:44:21 +0530

    newtest

--- public/electron.js
+++ public/electron.js
@@ -4,7 +4,7 @@ const { ipcMain } = require("electron");
 
 const url = require('url');
 const path = require('path');
-
+// Creates a secure Electron window titled 'WorkFaster' with 400x400 dimensions and security features
 function createMainWindow() {
     const mainWindow = new BrowserWindow({
         title: 'WorkFaster',
@@ -13,8 +13,10 @@ function createMainWindow() {
         frame: false,
         titleBarStyle: 'hidden',
         webPreferences: {
-            preload: path.join(__dirname, "preload.js"), // Path to preload script
-            contextIsolation: true,   // Keeps context isolated for security
-            nodeIntegration: false,   // Disables Node.js in the renderer (security best practice)
+            preload: path.join(__dirname, "preload.js"),
+            contextIsolation: true,
+            nodeIntegration: false,
         }
-    });
+    })
+};
+
